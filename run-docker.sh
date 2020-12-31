3f5db398c92f9d91f4a30e17fa0f1cc945e42afa
JGWill <jgi@jgwill.com>
2020-12-31 03:40:33 -0500

    add: stuff from training

--- run-docker.sh
+++ run-docker.sh
@@ -1,2 +1,4 @@
-nvidia-docker  run -it -d --rm -v $(pwd):/work -v /a/lib:/a/lib -v /a/model:/a/model jgwill/pix2pix:gia-pix2pix-tensorflow-memo bash
+docker pull jgwill/pix2pix:gia-pix2pix-tensorflow-memo;
+
+nvidia-docker  run -it -d  --rm -p "6006:6006" -v $(pwd):/work -v /a/lib:/a/lib -v /a/model:/a/model jgwill/pix2pix:gia-pix2pix-tensorflow-memo
 
--- sample-train.sh
+++ sample-train.sh
@@ -2,3 +2,6 @@ python pix2pix.py --mode train --output_dir facades_trained --max_epochs 200 --i
 #Train /a/lib/datasets/gia-young-picasso-v04-2012211928...
 python pix2pix.py --mode train --output_dir /a/model/models/model_giapicallo_v04__201221-pix2pix-2012300846 --max_epochs 200 --input_dir /a/lib/datasets/gia-young-picasso-v04-2012211928_1024_p2p_canny/train --which_direction BtoA
 ----
+
+#gia-yp-tii-composite-v04-v05-2012310103
+python pix2pix.py --mode train --output_dir /a/model/models/model_gia-yp-tii-composite-v04-v05-2012310103 --max_epochs 300 --input_dir /a/lib/datasets/gia-yp-tii-composite-v04-v05-2012310103/train --which_direction BtoA
