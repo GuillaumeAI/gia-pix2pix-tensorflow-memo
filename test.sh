6519af2e8ba04dc83b39b9382fb9c88022309d74
JGWill <jgi@jgwill.com>
2020-12-30 23:43:52 -0500

    add:test script

---
+++ test.sh
@@ -0,0 +1,7 @@
+python pix2pix.py \
+	--mode test \
+	--output_dir "/a/lib/datasets/gia-young-picasso-v04-2012211928_1024_p2p_canny/test" \
+	--input_dir "/a/lib/samples/sketches" \
+	--checkpoint "/a/model/models/model_giapicallo_v04__201221-pix2pix-2012300846"
+
+
