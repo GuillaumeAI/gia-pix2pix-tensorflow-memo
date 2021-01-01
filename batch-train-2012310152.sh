16d5f393cbc542fc53dfd15968245e68e5c6f8f5
JGWill <jgi@jgwill.com>
2021-01-01 04:54:15 -0500

    upgraded

--- batch-train-2012310152.sh
+++ batch-train-2012310152.sh
@@ -1,10 +1,22 @@
-#few frame of the new model to see if that works fine
-python pix2pix.py --mode train --output_dir /a/model/models/model_gia-yp-tii-composite-v04-v05-2012310103 --max_epochs 35 --input_dir /a/lib/datasets/gia-yp-tii-composite-v04-v05-2012310103/train --which_direction BtoA  --checkpoint /a/model/models/model_giapicallo_v04__201221-pix2pix-2012300846  --display_freq 50
+# Retrain the v01 by increasing resolution to 512 (CROP_SIZE)
+model_dir="/a/model/models/model_giapicallo_v04__201221-pix2pix-2012300846-512px"
+input_dir="/a/lib/datasets/gia-young-picasso-v04-2012211928_1024_p2p_canny/train"
+mkdir -p $model_dir
+python pix2pix.py --mode train --output_dir $model_dir --max_epochs 250 --input_dir $input_dir --which_direction BtoA --checkpoint $model_dir  --display_freq 100
 
 # Continue v01 to observe tomorrow what 50 EPOCS does in Qualities
-python pix2pix.py --mode train --output_dir /a/model/models/model_giapicallo_v04__201221-pix2pix-2012300846 --max_epochs 50 --input_dir /a/lib/datasets/gia-young-picasso-v04-2012211928_1024_p2p_canny/train --which_direction BtoA --checkpoint /a/model/models/model_giapicallo_v04__201221-pix2pix-2012300846/  --display_freq 50
+model_dir="/a/model/models/model_giapicallo_v04__201221-pix2pix-2012300846-512px"
+input_dir="/a/lib/datasets/gia-young-picasso-v04-2012211928_1024_p2p_canny/train"
+#mkdir -p $model_dir
+#python pix2pix.py --mode train --output_dir $model_dir --max_epochs 250 --input_dir $input_dir --which_direction BtoA --checkpoint $model_dir  --display_freq 100
+
+#python pix2pix.py --mode train --output_dir /a/model/models/model_giapicallo_v04__201221-pix2pix-2012300846 --max_epochs 50 --input_dir /a/lib/datasets/gia-young-picasso-v04-2012211928_1024_p2p_canny/train --which_direction BtoA --checkpoint /a/model/models/model_giapicallo_v04__201221-pix2pix-2012300846/  --display_freq 50
 
 # Continue model v02 for a while 
-python pix2pix.py --mode train --output_dir /a/model/models/model_gia-yp-tii-composite-v04-v05-2012310103 --max_epochs 355 --input_dir /a/lib/datasets/gia-yp-tii-composite-v04-v05-2012310103/train --which_direction BtoA --checkpoint /a/model/models/model_giapicallo_v04__201221-pix2pix-2012300846  --display_freq 50
+model_dir="/a/model/models/model_gia-yp-tii-composite-v04-v05-2012310103-512px"
+input_dir="/a/lib/datasets/gia-yp-tii-composite-v04-v05-2012310103/train"
+mkdir -p $model_dir
+python pix2pix.py --mode train --output_dir $model_dir --max_epochs 250 --input_dir $input_dir --which_direction BtoA --checkpoint $model_dir  --display_freq 100
+#python pix2pix.py --mode train --output_dir /a/model/models/model_gia-yp-tii-composite-v04-v05-2012310103 --max_epochs 355 --input_dir /a/lib/datasets/gia-yp-tii-composite-v04-v05-2012310103/train --which_direction BtoA --checkpoint /a/model/models/model_giapicallo_v04__201221-pix2pix-2012300846  --display_freq 50
 
 
