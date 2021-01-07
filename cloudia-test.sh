cf69b69cf5b8381629abf8620d47f6ac3a2efa75
jgi <jgi@jgwill.com>
2021-01-07 16:50:48 +0000

    chg:cloudia test

---
+++ cloudia-test.sh
@@ -0,0 +1,1 @@
+python pix2pix.py  --mode test  --output_dir "/a/lib/results/tii-2101071146" --input_dir "/a/lib/samples/gia-young-picasso-v04-2012211928_1024_p2p_canny-val"  --checkpoint "/a/model/models/ --checkpoint "/a/mode"
