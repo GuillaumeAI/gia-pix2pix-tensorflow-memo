1fbef221f0c8783769d0e7d0499b070389be7d13
jgi <jgi@jgwill.com>
2021-01-07 16:41:17 +0000

    chg:container name and exec

--- run-docker.sh
+++ run-docker.sh
@@ -1,7 +1,8 @@
-docker pull jgwill/pix2pix:gia-pix2pix-tensorflow-memo;
-export mount_root=/a
-export container_tag=jgwill/pix2pix:gia-pix2pix-tensorflow-memo
 
-nvidia-docker  run -it -d  --rm -p "6006:6006" -p "8000:8000" -p "8080:8080" -v $(pwd):/work -v $mount_root/lib:/a/lib -v $mount_root/model:/a/model $container_tag
+export mount_root=/a
+export container_tag=jgwill/pix2pix:gia-pix2pix-tensorflow-memo-2101071007
+docker pull $container_tag
+
+docker  run -it -d  --rm -p "6006:6006" -p "8000:8000" -p "8080:8080" -v $(pwd):/work -v $mount_root/lib:/a/lib -v $mount_root/model:/a/model $container_tag
 
 
