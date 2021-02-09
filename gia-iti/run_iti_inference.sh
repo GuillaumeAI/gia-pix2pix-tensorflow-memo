1288fd8ce919c560ff2c6fd20616278396b752cc
Jean Guillaume <jgi@jgwill.com>
2021-02-09 00:41:48 -0500

    chg:adding implementation

--- gia-iti/run_iti_inference.sh
+++ gia-iti/run_iti_inference.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 #run_iti_inference.sh
 ## ENV AND ARGS
+pix2pix_script=pix2pix.py
+
 python_interpreter=python
+
 checkpoint_dir=/checkpoint
 
 source_file_name_only=$1
@@ -12,14 +15,18 @@ source_dir=$model_dir/input
 target_dir=/out
 work_dir=/iti
 dim=2048
+scale_size=$dim
+
 preped_name=01_preped
 infered_dir=/infered
 #init
 mkdir -p $work_dir $infered_dir
 indir_name=00_indir
 indir=$work_dir/$indir_name
-prepeddir=$work_dir/$preped_name
-mkdir -p $indir $prepeddir
+preped_dir=$work_dir/$preped_name
+infered_name=02_infered
+infered_dir=$work_dir/$infered_name
+mkdir -p $indir $preped_dir
 
 cp $source_dir/$source_file_name_only $indir
 echo "---listing $indir ----------" > /out/indir.txt
@@ -28,16 +35,16 @@ echo cp $source_dir/$source_file_name_only $indir >> /out/indir.txt
 #@STCGoal PREP - Create the Split input the test is requiring
 echo "---PWD----" >> /out/indir.txt
 pwd >> /out/indir.txt
-echo "python3 prep.py --root_path $work_dir --in_path $indir_name --out_path $preped_name --dim $dim" >> /out/indir.txt
+echo "$python_interpreter  prep.py --root_path $work_dir --in_path $indir_name --out_path $preped_name --dim $dim" >> /out/indir.txt
 $python_interpreter prep.py --root_path $work_dir --in_path $indir_name --out_path $preped_name --dim $dim
 
 ls $indir >> /out/indir.txt
 
 # Copy the isotheric subdir in a dir we control
-cp $prepeddir'_'*/*  $prepeddir
+cp $preped_dir'_'*/*  $preped_dir
 
-echo "------prep: $preped_name -  $prepeddir" >> /out/indir.txt
-ls $prepeddir >> /out/indir.txt
+echo "------prep: $preped_name -  $preped_dir" >> /out/indir.txt
+ls $preped_dir >> /out/indir.txt
 
 echo "----- /iti -------" >> /out/indir.txt
 ls /iti >> /out/indir.txt
@@ -51,29 +58,46 @@ ls /iti >> /out/indir.txt
 # Should have a preped file in /model/$preped_dir
 
 
-sleep 1
-echo "-----------Listing / for testing-----------">/out/ls_slash.txt
-ls />>/out/ls_slash.txt
 
-sleep 1
-echo "-----------Listing /checkpoint for testing-----------">/out/ls_cp.txt
-ls $checkpoint_dir >>/out/ls_cp.txt
-
-echo "------Testing INputs" > /out/input-output.txt
-echo $1 >> /out/input-output.txt
-echo $2 >> /out/input-output.txt
-
-echo "exiting, see $0 and keep coding it to continue the thing"
-echo "something in the input dir" >> /model/input/out.txt
-sleep 1
-exit
 #@STCGoal INFERENCE (thru Test)
-     python pix2pix.py \
-        --mode test \
-        --output_dir "$infered_dir" \
-        --input_dir "$preped_dir" \
-        --checkpoint "$checkpoint_dir"
+for direction in "AtoB" "BtoA"
+do
+    echo "----Direction: $direction : ">> /out/itilog.txt
+
+    infered_dir_target=$infered_dir'_'$direction
+
+    echo $python_interpreter $pix2pix_script \
+    --mode test \
+    --output_dir "$infered_dir_target" \
+    --input_dir "$preped_dir" \
+    --checkpoint "$checkpoint_dir" \
+    --which_direction $direction \
+    --scale_size $scale_size >> /out/itilog.txt
+
+    $python_interpreter $pix2pix_script\
+    --mode test \
+    --output_dir "$infered_dir_target" \
+    --input_dir "$preped_dir" \
+    --checkpoint "$checkpoint_dir" \
+    --which_direction $direction \
+    --scale_size $scale_size
+
+    d=/out/$direction
+    mkdir -p $d
+    cp $infered_dir_target/images/* $d
+
+    echo "---exporting render: $d----------">> /out/itilog.txt
+    echo "---------------------------">> /out/itilog.txt
+
+done
+
+
 
 #@STCGoal Split and export results
 
-source /model/split.sh /out/$target_file_name_only
+echo "--------infered : $infered_dir ------" > /out/infered.txt
+ls $infered_dir  >> /out/infered.txt
+
+#source /model/split.sh /out/$target_file_name_only
+
+sleep 100
